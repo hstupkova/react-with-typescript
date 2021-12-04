4feb18a0c1cf363d564d8b5c8547dc88c57be37c
Hana Stupková <hstupkova@gmail.com>
2021-12-04 20:37:09 +0100

    adding a todo via form submission

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
+import React, { useState } from 'react';
+
+import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/todo';
 
 function App() {
-  const todos = [
-    new Todo('learn React'),
-    new Todo('learn TypeScript')
-  ]
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (text: string) => {
+    const newTodo = new Todo(text);
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  };
 
   return (
     <div className="App">
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
